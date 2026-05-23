2f938eb046b8e5f605b8bd7cfb752c07e78973a9
reyes.427991 <reyes.427991@ANNEX-PC28>
2026-05-23 09:24:38 +0800

    WIP Cashier Hands-On Activity

---
+++ CashierApplication/Cashier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierApplication
+{
+    public class Cashier : UserAccount
+    {
+        private string departmet;
+
+        public Cashier(string name, string department, string userName, string password) 
+            : base(name, userName, password)
+        { 
+            this.departmet = department;
+        }
+        public override bool validateLogin(string userName, string password)
+        {
+            return (user_name == userName && user_password  == password);
+        }
+
+    }
+}
---
+++ CashierApplication/UserAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashierApplication
+{
+    public class UserAccount
+    {
+        private string full_name;
+        protected string user_name;
+        protected string user_password;
+
+        public UserAccount(string name, string userName, string password) { 
+            full_name = name;
+            user_name = userName;
+            user_password = password;
+        }
+
+        public virtual bool validateLogin(string userName, string password)
+        {
+            return (user_name == userName && user_password == password);
+        }
+
+        public string getFullName()
+        {
+            return full_name;
+        }
+    }
+}
